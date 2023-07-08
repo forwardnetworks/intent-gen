04c7b0267766545f2540c52261d31a886203b2d1
Gary Berger <garyberger@forwardnetworks.com>
2023-07-08 11:05:24 -0400

    Update

--- intent_generator.py
+++ intent_generator.py
@@ -9,51 +9,37 @@ options = {
   "intent": "PREFER_DELIVERED",
   "maxCandidates": 5000,
   "maxResults": 1,
-  "maxReturnPathResults": 0,
+  "maxReturnPathResults": 1,
   "maxSeconds": 30,
   "maxOverallSeconds": 300,
   "includeNetworkFunctions": False
 }
 
-def pretty_print_dict(dictionary, indent=0):
-    for key, value in dictionary.items():
-        if isinstance(value, dict):
-            print(' ' * indent + str(key) + ':')
-            pretty_print_dict(value, indent + 4)
-        else:
-            print(' ' * indent + str(key) + ': ' + str(value))
-
 def remove_columns(df, columns):
     return df.drop(columns, axis=1)
 
-def check_key_exists(df, key):
-    return df.apply(lambda row: key in row.keys(), axis=1)
-
-def check_empty_array(df, column_name):
-    return df[column_name].apply(lambda x: len(x) == 0)
-
-def copy_columns(df1,df2, columns):
-    df2[columns] = df1[columns].copy()
-
-def process_object(obj):
-    print(obj)
-    print() # print a new line just as a record separator
 
 def check_info_paths(data):
     for element in data:
-        if 'info' in element and 'paths' in element['info'] and not element['info']['paths']:
-            element['hopfix'] = len(element['info']['paths'])
-        if (
-            'returnPathInfo' not in element
-            or 'paths' not in element['returnPathInfo']
-            or element['returnPathInfo']['paths']
-        ):
-            element['hopfix'] = len(element['info']['paths'])
-        element['hopfix'] = len(element['info']['paths'])
-        element['hopfix'] = len(element['info']['paths'])
+        if 'info' in element and 'paths' in element['info']:
+            if not element['info']['paths']:
+                element['info']['paths'] = [{'forwardingOutcome': 'NOTDELIVERED', 'securityOutcome': 'UNKNOWN', 'hops': []}]
+            else:
+                element['pathCount'] = len(element['info']['paths'])
+                element['forwardHops'] = len(element['info']['paths'][0]['hops'])
+        else:
+            element['info'] = {'paths': []}
+        
+        if 'returnPathInfo' in element and 'paths' in element['returnPathInfo']:
+            if element['returnPathInfo']['paths']:
+                element['returnHops'] = len(element['returnPathInfo']['paths'][0]['hops'])
+                element['returnPathCount'] = len(element['returnPathInfo']['paths'])
+            else:
+                element['returnPathInfo']['paths'] = [{'forwardingOutcome': 'NOTDELIVERED', 'securityOutcome': 'UNKNOWN', 'hops': []}]
+        else:
+            element['returnPathInfo'] = {'paths': []}
+
     return data
-
-
 
 # Get the username and password from environment variables.
 username = os.getenv('FWD_USER')
@@ -100,12 +86,10 @@ async def process_input(obj):
                         "ipProto": ipProto,
                         "dstPort":  dstPorts
                     }
-                    queries['hopfix'] = (query)
+                    queries.append(query)
 
             query_list_df = pd.DataFrame(queries)
-            # print(query_list_df)
-            # print(queries)
-            # Call request.post for each region using aiohttp
+            query_list_df['region'] = region
             body =  {"queries": queries, **options}
             response_text, response_status = await fetch(session, 'https://fwd.app/api/snapshots/627174/pathsBulkSeq', body)
 
@@ -118,51 +102,18 @@ async def process_input(obj):
             # Cleanup for dataframe import
             fix_data = check_info_paths(parsed_data)
 
-            print(fix_data)
-
             r = pd.json_normalize(
                 fix_data, 
-                # record_path=['info', 'paths'],
-                # meta=['hopfix''],
-                # errors='ignore'
+                record_path=['info', 'paths'],
+                meta=['forwardHops','returnHops', 'pathCount', 'returnPathCount','queryUrl'],
+                errors='ignore'
             )
             merged_df = pd.merge(r, query_list_df, left_index=True, right_index=True)
-            print(merged_df.columns)
-            print(f"-> {merged_df}")
+            newdf = remove_columns(merged_df, ['hops'])
+            reorderdf = newdf[['region', 'srcIp', 'dstIp', 'ipProto', 'dstPort',  'forwardingOutcome', 'securityOutcome', 'pathCount', 'forwardHops', 'returnPathCount','returnHops', 'queryUrl']]
+            print(f"-> {reorderdf}")
+            reorderdf.to_excel(f"{region}.xlsx", index=True)
 
-            # for index, row in merged_df.iterrows():
-            #     if (len(row['info.paths']) == 0):
-            #         print(f"found error {index}")
-            #         merged_df.at[index, 'forwardOutcome'] = "NOPATH"
-            #         merged_df.at[index, 'securityOutcome'] = "UNKNOWN"
-            #         print(merged_df)
-            #     # copy_columns(merged_df,df, ['srcIp','dstIp','ipProto', 'dstPort','forwardOutcome','securityOutcome'])
-            #     else:
-            #         merged_df.at[index, 'forwardOutcome'] = json.dumps(row['info.paths'])
-            #         print(row['info.paths'])
-
-            # print(merged_df.columns)
-            # print(merged_df)
-
-            # # json_data = merged_df.to_json(orient='records')
-
-            # r = pd.json_normalize(
-            #     parsed_data,
-            #     record_path=['info', 'paths'],
-            #     # meta=['srcIpLocationType','dstIpLocationType']
-            #     # errors='ignore'
-            # )
-            # merged_df = pd.merge(r, query_list_df, left_index=True, right_index=True) 
-            # print(merged_df.columns)
-            # print(merged_df)
-
-        
-        # return parsed_data,query_list_df
-
-# Process input data
-
-# pd.set_option('display.max_rows', None)
-# pd.set_option('display.max_columns', None)
-
+# main
 loop = asyncio.get_event_loop()
 loop.run_until_complete(process_input(data))
